aacb6a353b2d111edbaae4475ac5e19b6f549a78
1Chris-Saylor <139816219+1Chris-Saylor@users.noreply.github.com>
2024-01-06 02:04:53 -0800

    Animated button
    Completed and its working. Done in both CSS and jQuery

--- js/index.js
+++ js/index.js
@@ -1,21 +1,31 @@
-// index.js
+$(document).ready(function() {
+    // Define the transition duration
+    var transitionDuration = '.4s';
 
-console.log("Your index.js file is loaded correctly!!!! NEW");
+    // Add hover effect to elements with the class "btn-hover"
+    $(".btn-hover").hover(
+        function() { // mouseenter
+            $(this).css({
+                'background-position': '100% 0',
+                'transition': 'all ' + transitionDuration + ' ease-in-out',
+            });
+        },
+        function() { // mouseleave
+            $(this).css({
+                'background-position': '0 0',
+                'transition': 'all ' + transitionDuration + ' ease-in-out',
+            });
+        }
+    );
 
-// Other JavaScript code can go here
+    // Remove focus outline on button click
+    $(".btn-hover").focus(function() {
+        $(this).blur();
+    });
 
-
-
-// js/index.js
-
-// Test the event listener with a console.log
-console.log("Hello world!");
-
-// Add an event listener for the element with the ID "myID"
-$("#myID").on("click", function() {
-    console.log("You clicked on #myID");
-
-    // Your jQuery code to manipulate the UI
-    $(".fadeMe").fadeToggle("slow");
+    // Apply specific styles for the "color-3" variant
+    $(".btn-hover.color-3").css({
+        'background-image': 'linear-gradient(to right, #667eea, #764ba2, #6B8DD6, #8E37D7)',
+        'box-shadow': '0 4px 15px 0 rgba(116, 79, 168, 0.75)',
+    });
 });
-
